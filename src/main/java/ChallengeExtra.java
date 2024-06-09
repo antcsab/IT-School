49a6684fe207ccc52b0c6001f4ae317b0352bfbb
Antal Csaba <antcsab@gmail.com>
2024-06-09 17:38:22 +0300

    challange1 extra

---
+++ src/main/java/ChallengeExtra.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class ChallengeExtra{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int Number1 = scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int Number2 = scanner.nextInt();
+        int addition = Number1 + Number2;
+        System.out.println("The addition of the numbers is: " + addition);
+        scanner.close();
+    }
+}
