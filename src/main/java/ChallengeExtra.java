34de81fae4ccdea40bc946e4415294dc7ef1d642
Antal Csaba <antcsab@gmail.com>
2024-06-10 17:24:43 +0300

    Substractions of the numbers

--- src/main/java/ChallengeExtra.java
+++ src/main/java/ChallengeExtra.java
@@ -10,7 +10,9 @@ public class ChallengeExtra{
         System.out.println("Enter the second number: ");
         int Number2 = scanner.nextInt();
         int addition = Number1 + Number2;
+        int minus = Number1 - Number2;
         System.out.println("The addition of the numbers is: " + addition);
+        System.out.println("The minus of the numbers is: " + minus);
         scanner.close();
     }
 }
