38c7f9226dc2355affbe7197771b7a348a299378
Antal Csaba <antcsab@gmail.com>
2024-06-18 06:09:09 +0300

    Session 4 - java operators and loops

---
+++ src/main/java/session_4_java_operators_and_loops/challenges/Challenge4_8.java
@@ -0,0 +1,17 @@
+package session_4_java_operators_and_loops.challenges;
+
+//Write a Java program to calculate the factorial of a number using a for loop. Declare an integer variable number and
+// assign a value to it. Then calculate and print the factorial number.
+
+public class Challenge4_8 {
+    public static void main(String[] args) {
+        int fact = 1;
+        for (int i = 0; i < 24; i++) {;
+        fact *= i;
+            {
+                System.out.println("The factorial of a number is: " + fact);
+            }
+        }
+
+    }
+}
